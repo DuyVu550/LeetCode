98ec655a3399e37638e36a03ad911491822fad9c
DuyVu550 <duyvunguyen550@gmail.com>
2025-11-18 21:23:03 +0700

    Fix

--- src/main/java/com/example/demo/Contains_Duplicate.java
+++ src/main/java/com/example/demo/Contains_Duplicate.java
@@ -1,51 +1,13 @@
 package com.example.demo;
 
+import java.util.HashSet;
+
 public class Contains_Duplicate {
-    public static void main(String[] args) {
-        int[] arr = new int[] { -1, -2, -3, 4, 5, 6, 7, -2 };
-        boolean check = Check(arr);
-        System.out.print(check);
-    }
-
-    public static int Max(int[] arr) {
-        int max = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (max < arr[i]) {
-                max = arr[i];
-            }
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet();
+        for (int num : nums) {
+            set.add(num);
         }
-        return max;
+        return set.size() < nums.length;
     }
-
-    public static int Min(int[] arr) {
-        int min = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (min > arr[i]) {
-                min = arr[i];
-            }
-        }
-        return min;
-    }
-
-    public static int[] Count(int[] arr) {
-        int max = Max(arr);
-        int min = Min(arr);
-        int[] count = new int[max - min + 1];
-        for (int i = 0; i < arr.length; i++) {
-            count[arr[i] - min]++;
-        }
-        return count;
-    }
-
-    public static boolean Check(int[] arr) {
-        int[] count = Count(arr);
-        int min = Min(arr);
-        for (int i = 0; i < arr.length; i++) {
-            if (count[arr[i] - min] > 1) {
-                return true;
-            }
-        }
-        return false;
-    }
-
 }
--- src/main/java/com/example/demo/Rotate_Array.java
+++ src/main/java/com/example/demo/Rotate_Array.java
@@ -1,16 +1,23 @@
 package com.example.demo;
 
-import java.util.Arrays;
-
 public class Rotate_Array {
     public static void main(String[] args) {
         int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
-        int k = 3;
-        int[] a = Arrays.copyOfRange(arr, arr.length - k, arr.length);
-        int[] b = Arrays.copyOfRange(arr, 0, arr.length - k);
-        int[] merged = new int[a.length + b.length];
-        System.arraycopy(a, 0, merged, 0, a.length);
-        System.arraycopy(b, 0, merged, a.length, b.length);
-        System.out.println(Arrays.toString(merged));
+        int k = 4;
+        if (k < 0 || k > arr.length)
+            return;
+        Rotate(arr, 0, arr.length - 1);
+        Rotate(arr, 0, k - 1);
+        Rotate(arr, k, arr.length - 1);
+    }
+
+    public static void Rotate(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
