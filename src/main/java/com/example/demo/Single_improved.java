ce9c0533de970775c9460f032895d4eeceb752a4
DuyVu550 <duyvunguyen550@gmail.com>
2025-11-19 14:52:09 +0700

    fix by set

--- src/main/java/com/example/demo/Single_improved.java
+++ src/main/java/com/example/demo/Single_improved.java
@@ -1,19 +1,20 @@
 package com.example.demo;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Single_improved {
     public static void main(String[] args) {
-        int[] a = { 2, 1, 2, 54, 56, 55, 54, 56 };
+        int[] a = { 1, 2, 1, 2, 1, 2, 6, 6, 8, 9, 9, -2, 6 };
+        Set set = new HashSet();
+        Set set2 = new HashSet();
         for (int i = 0; i < a.length; i++) {
-            boolean check = true;
-            for (int j = 0; j < a.length; j++) {
-                if (i == j)
-                    continue;
-                if (a[i] == a[j]) {
-                    check = false;
-                    break;
-                }
+            if (!set.add(a[i])) {
+                set2.add(a[i]);
             }
-            if (check) {
+        }
+        for (int i = 0; i < a.length; i++) {
+            if (!set2.contains(a[i])) {
                 System.out.print(a[i] + " ");
             }
         }
